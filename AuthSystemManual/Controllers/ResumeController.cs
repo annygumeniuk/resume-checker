aeaed3391048108eaccfb78a716dd142d1f15bd2
annygumeniuk <anny.gumeniuk@gmail.com>
2024-05-01 10:56:34 +0300

    Added editing resume logic (seems to work fine)

--- AuthSystemManual/Controllers/ResumeController.cs
+++ AuthSystemManual/Controllers/ResumeController.cs
@@ -32,12 +32,48 @@ namespace ResumeCheckSystem.Controllers
                 ViewBag.UserEmail = userEmail;
                 ViewBag.FullName = $"{user.FirstName} {user.LastName}";
 
+                if (resume.Count == 0)
+                {
+                    ViewBag.Display = "0";
+                }
+
                 return View(resume);
             }
             else
             {
                 return RedirectToAction("Login", "Account");
             }
+        }
+
+        void AddSkillsToDb(IEnumerable<string> selectedSkills, IEnumerable<int> skillLevels, int? userId)
+        {
+            foreach (var pair in selectedSkills.Zip(skillLevels, (skill, level) => new { Skill = skill, Level = level }))
+            {
+                // Find the SkillId corresponding to the selected skill
+                var skillId = _context.Skill.FirstOrDefault(s => s.SkillName == pair.Skill)?.Id;
+
+                // If the skill exists, create a new UserSkill object and add it to the context
+                if (skillId != null)
+                {
+                    var userSkill = new UserSkill
+                    {
+                        SkillId = skillId.Value,
+                        SkillLevel = pair.Level
+                    };
+
+                    _context.UserSkill.Add(userSkill);
+                    _context.SaveChanges();
+
+                    var resume = new Resume
+                    {
+                        UserId = Convert.ToInt32(userId),
+                        // Use the UserSkill ID instead of SkillId
+                        UserSkillId = userSkill.Id
+                    };
+                    _context.Resume.Add(resume);
+                }
+            }
+            _context.SaveChanges();
         }
 
         // CREATE RESUME
@@ -65,35 +101,7 @@ namespace ResumeCheckSystem.Controllers
             HttpContext httpContext = HttpContext;
             int? userId = SessionHelper.GetUserId(httpContext);
 
-            foreach (var pair in selectedSkills.Zip(skillLevels, (skill, level) => new { Skill = skill, Level = level }))
-            {
-                // Find the SkillId corresponding to the selected skill
-                var skillId = _context.Skill.FirstOrDefault(s => s.SkillName == pair.Skill)?.Id;
-
-                // If the skill exists, create a new UserSkill object and add it to the context
-                if (skillId != null)
-                {
-                    var userSkill = new UserSkill
-                    {
-                        SkillId = skillId.Value,
-                        SkillLevel = pair.Level
-                    };
-
-                    _context.UserSkill.Add(userSkill);
-                    _context.SaveChanges();
-
-                    var resume = new Resume
-                    {
-                        UserId = Convert.ToInt32(userId),
-                        // Use the UserSkill ID instead of SkillId
-                        UserSkillId = userSkill.Id
-                    };
-                    _context.Resume.Add(resume);
-                }
-            }
-
-            // Save changes to the database
-            _context.SaveChanges();
+            AddSkillsToDb(selectedSkills, skillLevels, userId);
 
             return RedirectToAction("Index"); // redirecting to page with resume displayed
         }
@@ -146,8 +154,6 @@ namespace ResumeCheckSystem.Controllers
              .Include(x => x.UserSkill)
              .ToList();
 
-
-
             // Passing data to the page
             ViewBag.SkillFromResume = resumeSkills;
             ViewBag.SkillsByCategory = skillsByCategory;
@@ -156,9 +162,21 @@ namespace ResumeCheckSystem.Controllers
             return View(categoryData);           
         }
         
+        // PROCESSING EDITING FORM
         [HttpPost]
-        public IActionResult EditResumeForm()
+        public IActionResult EditResumeForm(IEnumerable<string> selectedSkills, IEnumerable<int> skillLevels)
         {
+            HttpContext httpContext = HttpContext;
+            int? userId = SessionHelper.GetUserId(httpContext);
+            // Get resume skills for the current user
+            var resumeSkills = _context.Resume
+             .Where(x => x.UserId == userId)
+             .Include(x => x.UserSkill)
+             .ToList();
+                        
+            _context.Database.ExecuteSqlRaw("DELETE FROM Resume WHERE UserId = {0}", userId);
+            AddSkillsToDb(selectedSkills, skillLevels, userId);            
+
             return RedirectToAction("Index", "Home");
         }
     }
